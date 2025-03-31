d166c4eb7d7664417ee74a1047e7df8cc0c3d91d
Pawel968 <cabanpawel96@gmail.com>
2025-03-31 20:36:42 +0200

    Stworzenie strony

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,42 @@
 import './App.css'
 
+const n_k = 1;
+let x;
+let y;
+console.log(n_k);
+
 function App() {
 
   return (
-    <>Hello, World!</>
+    <>
+    
+  <div class="g"> 
+  <div class="btext">
+  <h1>Komputer ma numer - {n_k}</h1>
+  </div>
+  </div>
+  <br/>
+  <br/>
+  <div class="text">
+  </div>
+  <br/>
+  <br/>
+  <div class="gg">
+    {n_k} * 1 = {n_k*1} <br/>
+    {n_k} * 2 = {n_k*2} <br/>
+    {n_k} * 3 = {n_k*3} <br/>
+    {n_k} * 4 = {n_k*4} <br/>
+    {n_k} * 5 = {n_k*5} <br/>
+    {n_k} * 6 = {n_k*6} <br/>
+    {n_k} * 7 = {n_k*7} <br/>
+    {n_k} * 8 = {n_k*8} <br/>
+    {n_k} * 9 = {n_k*9} <br/>
+    {n_k} * 10 = {n_k*10} <br/>
+  </div>
+
+    </>
   )
 }
 
 export default App
+
